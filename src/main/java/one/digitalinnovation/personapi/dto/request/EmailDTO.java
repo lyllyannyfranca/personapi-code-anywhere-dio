bcaab17e91c754ab4e2495df2313600a7b5643b6
Lyllyanny França <lyllyanny.franca@gmail.com>
2021-06-26 21:03:22 -0300

    Improved person creation functionality

---
+++ src/main/java/one/digitalinnovation/personapi/dto/request/EmailDTO.java
@@ -0,0 +1,28 @@
+package one.digitalinnovation.personapi.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import one.digitalinnovation.personapi.enums.EmailType;
+
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class EmailDTO {
+
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    private EmailType type;
+
+    @NotEmpty
+    @Size(max = 255)
+    private String email;
+}
